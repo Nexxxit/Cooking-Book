e9c0eacf9a1afa8b3019fbefb1b162b055f48ed7
Nexxxit <varsik.arsik@mail.ru>
2026-01-22 18:05:43 +0500

    feat: add footer to main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/widgets/header/ui/Header";
 import { Providers } from "./providers";
 import { siteConfig } from "@/shared/config/site.config";
+import Footer from "@/widgets/footer/ui/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,8 +32,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <Header />
-          {children}
+          <div className="flex flex-col h-dvh">
+            <Header />
+            <main className="flex-1 min-h-0">{children}</main>
+            <Footer />
+          </div>
         </Providers>
       </body>
     </html>
